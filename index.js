d1ef5fe169ca2522c6e09cd9fbf7de1c716c4e51
Trenton <trentonnesbitt@outlook.com>
2018-08-21 16:52:44 -0700

    adding git ignore and connecting to mongo db

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 const app = express();
+
+//connecting to mongo db:
+mongoose.connect('keys.mongoURI');
+
 
 app.get('/',(req,res) => {
     res.send('Hello');
